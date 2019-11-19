adde2970e3bc4541e6b693a34a9880ec3154ec0e
Nemanja-Danev <nemanja.danev@green-adv.rs>
2019-11-19 11:45:20 +0100

    fixiran events page

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -12,14 +12,36 @@
 
         .events_section{
             padding-top: 50px;
-            padding-bottom: 80px;
+            padding-bottom: 100px;
         }  
+
+        .event-card {
+            padding-top: 75px;
+            padding-bottom: 75px;
+            border-bottom: 1px solid #a7a9ac;
+        }
+
+        .event-card p {
+            font-size: 16px;
+            font-weight: 400;
+        }
+
+        .events_section .event-card:last-child {
+            border-bottom: none;
+        }
 
         .events_section a{
             font-size: 20px;
             background-color: #f36f46;
             color: #fff;
             padding: 8px 35px;
+            text-decoration: none;
+            transition: all .4s;
+        }
+
+        .events_section a:hover {
+            background-color: #08090b;
+            color: #f1592a;
         }
 
         .previosuBtn{
@@ -27,6 +49,19 @@
             color: #fff;
             font-weight: bold;
             padding: 15px 50px;
+        }
+
+        .previous-events-title {
+            background-color: #f1592a;
+            display: inline-block;
+            padding: 10px 50px;
+            color: #fff;
+            margin-top: 50px;
+            
+        }
+
+        .previous-events-title h2 {
+            font-size: 1.75rem;
         }
 
     </style>
@@ -43,7 +78,7 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-12">
-                                <h1 class="text-white">EVENTS</h1>
+                                <h1>EVENTS</h1>
                             </div>
                         </div>
                     </div>
@@ -54,95 +89,105 @@
 
         <div class="container events_section">
 
-            <div class="row" style="border-bottom: 1px solid #a7a9ac; padding-bottom: 40px; margin-top: 30px;">
-                <div class="col-12 col-md-2">
-                    <h3 class="orange_text">16<br>DEC<br>2019</h3>
-                </div>
-                <div class="col-12 col-md-4">
-                    <h3>GDPR Konferencija 2019 Podgorica</h3><br>
-                    <p><span><img src="{{ asset('images/web/clock.png') }}"></span> 8:00am - 5:00pm</p>
-                    <br>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Esse, veritatis optio quo similique facere, non laboriosam itaque 
-                        nulla dolor quas fuga iure a eligendi ipsa odit est fugit. Ut, inventore?
-                    </p>
-                    <br>
-                    <a href="#">VIEW DETAILS</a>
-                </div>
-                <div class="col-12 col-md-6">
-                    <img class="img-fluid" src="{{ asset('images/web/events.png') }}">
-                </div>
-            </div>
-
-            <div class="row" style="border-bottom: 1px solid #a7a9ac; padding-bottom: 40px; margin-top: 30px;">
-                <div class="col-12 col-md-2">
-                    <h3 class="orange_text">16<br>DEC<br>2019</h3>
-                </div>
-                <div class="col-12 col-md-4">
-                    <h3>GDPR Konferencija 2019 Podgorica</h3><br>
-                    <p><span><img src="{{ asset('images/web/clock.png') }}"></span> 8:00am - 5:00pm</p>
-                    <br>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Esse, veritatis optio quo similique facere, non laboriosam itaque 
-                        nulla dolor quas fuga iure a eligendi ipsa odit est fugit. Ut, inventore?
-                    </p>
-                    <br>
-                    <a href="#">VIEW DETAILS</a>
-                </div>
-                <div class="col-12 col-md-6">
-                    <img class="img-fluid" src="{{ asset('images/web/events.png') }}">
+            <div class="event-card">
+                <div class="row">
+                    <div class="col-12 col-md-1">
+                        <h3 class="orange_text">16<br>DEC<br>2019</h3>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <h4 class="mb-3">GDPR Konferencija 2019 Podgorica</h4>
+                        <p class="time-icon"><span><img src="{{ asset('images/web/clock.png') }}"></span> 8:00am - 5:00pm</p>
+                        <br>
+                        <p class="my-3">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Esse, veritatis optio quo similique facere, non laboriosam itaque 
+                            nulla dolor quas fuga iure a eligendi ipsa odit est fugit. Ut, inventore?
+                        </p>
+                        <br>
+                        <a href="#">VIEW DETAILS</a>
+                    </div>
+                    <div class="col-12 col-md-7">
+                        <img class="img-fluid" src="{{ asset('images/web/events.png') }}">
+                    </div>
                 </div>
             </div>
 
-            <div class="row" style="border-bottom: 1px solid #a7a9ac; padding-bottom: 40px; margin-top: 30px;">
-                <div class="col-12 col-md-2">
-                    <h3 class="orange_text">16<br>DEC<br>2019</h3>
+            <div class="event-card">
+                <div class="row">
+                    <div class="col-12 col-md-1">
+                        <h3 class="orange_text">16<br>DEC<br>2019</h3>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <h4 class="mb-3">GDPR Konferencija 2019 Podgorica</h4>
+                        <p class="time-icon"><span><img src="{{ asset('images/web/clock.png') }}"></span> 8:00am - 5:00pm</p>
+                        <br>
+                        <p class="my-3">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Esse, veritatis optio quo similique facere, non laboriosam itaque 
+                            nulla dolor quas fuga iure a eligendi ipsa odit est fugit. Ut, inventore?
+                        </p>
+                        <br>
+                        <a href="#">VIEW DETAILS</a>
+                    </div>
+                    <div class="col-12 col-md-7">
+                        <img class="img-fluid" src="{{ asset('images/web/events.png') }}">
+                    </div>
                 </div>
-                <div class="col-12 col-md-4">
-                    <h3>GDPR Konferencija 2019 Podgorica</h3><br>
-                    <p><span><img src="{{ asset('images/web/clock.png') }}"></span> 8:00am - 5:00pm</p>
-                    <br>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Esse, veritatis optio quo similique facere, non laboriosam itaque 
-                        nulla dolor quas fuga iure a eligendi ipsa odit est fugit. Ut, inventore?
-                    </p>
-                    <br>
-                    <a href="#">VIEW DETAILS</a>
-                </div>
-                <div class="col-12 col-md-6">
-                    <img class="img-fluid" src="{{ asset('images/web/events.png') }}">
+            </div>
+
+            <div class="event-card">
+                <div class="row">
+                    <div class="col-12 col-md-1">
+                        <h3 class="orange_text">16<br>DEC<br>2019</h3>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <h4 class="mb-3">GDPR Konferencija 2019 Podgorica</h4>
+                        <p class="time-icon"><span><img src="{{ asset('images/web/clock.png') }}"></span> 8:00am - 5:00pm</p>
+                        <br>
+                        <p class="my-3">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Esse, veritatis optio quo similique facere, non laboriosam itaque 
+                            nulla dolor quas fuga iure a eligendi ipsa odit est fugit. Ut, inventore?
+                        </p>
+                        <br>
+                        <a href="#">VIEW DETAILS</a>
+                    </div>
+                    <div class="col-12 col-md-7">
+                        <img class="img-fluid" src="{{ asset('images/web/events.png') }}">
+                    </div>
                 </div>
             </div>
 
 
-            <div class="row" style="margin-top: 50px; margin-bottom: 50px;">
+            <div class="row">
                 <div class="col-12 text-center">
-                    <button class="btn btn-default previosuBtn">Previous events</button>
+                    <div class="previous-events-title">
+                        <h2>Previous events</h2>
+                    </div>
                 </div>
             </div>
 
 
-            <div class="row" style="border-bottom: 1px solid #a7a9ac; padding-bottom: 40px; margin-top: 30px;">
-                <div class="col-12 col-md-2">
-                    <h3 class="orange_text">16<br>DEC<br>2019</h3>
-                </div>
-                <div class="col-12 col-md-4">
-                    <h3>GDPR Konferencija 2019 Podgorica</h3><br>
-                    <p><span><img src="{{ asset('images/web/clock.png') }}"></span> 8:00am - 5:00pm</p>
-                    <br>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Esse, veritatis optio quo similique facere, non laboriosam itaque 
-                        nulla dolor quas fuga iure a eligendi ipsa odit est fugit. Ut, inventore?
-                    </p>
-                    <br>
-                    <a href="#">VIEW DETAILS</a>
-                </div>
-                <div class="col-12 col-md-6">
-                    <img class="img-fluid" src="{{ asset('images/web/events.png') }}">
+            <div class="event-card">
+                <div class="row">
+                    <div class="col-12 col-md-1">
+                        <h3 class="orange_text">16<br>DEC<br>2019</h3>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <h4 class="mb-3">GDPR Konferencija 2019 Podgorica</h4>
+                        <p class="time-icon"><span><img src="{{ asset('images/web/clock.png') }}"></span> 8:00am - 5:00pm</p>
+                        <br>
+                        <p class="my-3">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Esse, veritatis optio quo similique facere, non laboriosam itaque 
+                            nulla dolor quas fuga iure a eligendi ipsa odit est fugit. Ut, inventore?
+                        </p>
+                        <br>
+                        <a href="#">VIEW DETAILS</a>
+                    </div>
+                    <div class="col-12 col-md-7">
+                        <img class="img-fluid" src="{{ asset('images/web/events.png') }}">
+                    </div>
                 </div>
             </div>
 
