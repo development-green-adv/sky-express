74857d430e751311601eb2c499067a6d5e0b6391
Nemanja-Danev <nemanja.danev@green-adv.rs>
2019-11-26 10:13:42 +0100

    kerio stranica zavrsena

--- resources/views/kerio.blade.php
+++ resources/views/kerio.blade.php
@@ -80,37 +80,84 @@
         <div class="container-fluid avast-third-part eperi-third-part">
             <div class="container">
                 <div class="row">
-                    <div class="col-12 text-center">
-                        <h4 class="mb-5 text-white">HOW DOES YOUR ORGANIZATION BENEFIT <br> FROM EPERI CLOUD DATA PROTECTION?</h4>
+                    <div class="col-md-6">
+                        <div class="kerio-third-part-left">
+                            <h5 class="orange_text mb-2">Kerio Connect</h5>
+                            <p class="pr-5 mb-5"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <ul class="eperi-first-part-ul kerio-third-part-left-ul">
+                                <li class="mb-3">
+                                    <div class="row">
+                                        <div class="col-md-1">
+                                            <img src="{{ asset('images/web/correct.svg') }}" alt="">
+                                        </div>
+                                        <div class="col-md-11">
+                                            <p>Real time data encryption;</p>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="mb-3">
+                                    <div class="row">
+                                        <div class="col-md-1">
+                                            <img src="{{ asset('images/web/correct.svg') }}" alt="">
+                                        </div>
+                                        <div class="col-md-11">
+                                            <p>Only authorized users can access protected data;</p>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="mb-3">
+                                    <div class="row">
+                                        <div class="col-md-1">
+                                            <img src="{{ asset('images/web/correct.svg') }}" alt="">
+                                        </div>
+                                        <div class="col-md-11">
+                                            <p>Completely stand-alone security system without needs for any  adaptation of existing application, database or interface;</p>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="mb-3">
+                                    <div class="row">
+                                        <div class="col-md-1">
+                                            <img src="{{ asset('images/web/correct.svg') }}" alt="">
+                                        </div>
+                                        <div class="col-md-11">
+                                            <p>German quality;</p>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5">
+                        <div class="kerio-third-part-left">
+                            <h5 class="orange_text mb-2">Kerio Control</h5>
+                            <p class="pr-5 mb-5"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <p class="mb-4">Lorem Ipsum is simply dummy text of the printing</p>
+                            <p  class="mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+                        </div>
                     </div>
                 </div>
-                <div class="row mt-5">
-                    <div class="col-md-3">
-                        <div class="eperi-third-part-card">
-                            <img class="img-fluid office-365"  src="{{ asset('images/web/eperi-card-1.png') }}" alt="">
-                            <h6 class="orange_text mt-5 mb-3 pl-4">EPERI CLOUD DATA PROTECTION FOR OFFICE 365</h6>
-                            <p class="pr-3 pl-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                <div class="row row-kerio-third">
+                    <div class="col-md-6">
+                        <div class="kerio-card">
+                            <div class="kerio-card-content text-center pt-4">
+                                <h6 class="orange_text">Kerio<strong>Connect</strong></h6>
+                                <p class="my-2">MIL SERVER + MORE</p>
+                                <p class="px-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                            </div>
+                            <img class="pt-2" src="{{ asset('images/web/kario-card-icon.png') }}" alt="">
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <div class="eperi-third-part-card">
-                            <img class="img-fluid" src="{{ asset('images/web/eperi-card-2.png') }}" alt="">
-                            <h6 class="orange_text mt-5 mb-2 pl-4">EPERI CLOUD DATA PROTECTION FOR SALESFORCE</h6>
-                            <p class="pr-3 pl-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="eperi-third-part-card">
-                            <img class="img-fluid" src="{{ asset('images/web/eperi-card-3.png') }}" alt="">
-                            <h6 class="orange_text mt-5 mb-2 pl-4">EPERI CLOUD DATA PROTECTION FOR WEB APPLICATIONS</h6>
-                            <p class="pr-3 pl-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="eperi-third-part-card text-center">
-                            <img class="img-fluid eperi-gateway-logo" src="{{ asset('images/web/eperi-card-4.png') }}" alt="">
-                            <h6 class="orange_text mt-4 mb-4 eperi-gateway-title">THE EPERI GATEWAY</h6>
-                            <p class="pr-3 pl-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                    <div class="col-md-6">
+                        <div class="kerio-card">
+                            <div class="kerio-card-content text-center">
+                                <h6 class="orange_text">Kerio<strong>Control</strong></h6>
+                                <p class="my-2">FIREWALL + MORE</p>
+                                <p class="px-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                            </div>
+                            <img src="{{ asset('images/web/kario-card-icon-right.png') }}" alt="">
                         </div>
                     </div>
                 </div>
